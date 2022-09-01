bb4dfe9f7f17bc257c7f6c810b7406f1a3864160
VandelayUtd <drewhadley.guitar@gmail.com>
2022-09-01 17:14:34 -0400

    added package and started building out intial server code

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, ()=> {
+    console.log(`listening on port ${PORT}`)
+})

b1d7c5cfae9a076ba9cb10e8981f924b97cfc229
VandelayUtd <drewhadley.guitar@gmail.com>
2022-09-22 15:34:07 -0400

    fixed get request for launches and connected fronted with backend post request successfully

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -4,16 +4,15 @@ const {
  } = require('../../models/launches.model')
 
 function httpGetAllLaunches(req, res) {
-    return res.status(200).json(getAllLaunches);
+    return res.status(200).json(getAllLaunches());
 };
 
 function httpAddNewLaunch(req, res) { 
     const launch = req.body;
 
-    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.launchDate ) {
+    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.target ) {
         return res.status(400).json({
             error: 'missing required launch property',
-
         })
     }
 

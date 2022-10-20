1db40ff96261732f27d3245ad568b32ae189793f
VandelayUtd <drewhadley.guitar@gmail.com>
2022-10-20 16:30:30 -0400

    connected planets model with database

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { parse } = require('csv-parse');
 
-const habitablePlanets = [];
+const planets = require('./planets.mongo');
 
 function isHabitablePlanet(planet) {
   return planet['koi_disposition'] === 'CONFIRMED'
@@ -17,25 +17,41 @@ function loadPlanetsData() {
           comment: '#',
           columns: true,
         }))
-        .on('data', (data) => {
+        .on('data', async (data) => {
           if (isHabitablePlanet(data)) {
-            habitablePlanets.push(data);
+            // TOTO: REPLACE BELOW CREATE WITH UPSERT
+            savePlanet(data);
           }
         })
         .on('error', (err) => {
           console.log(err);
           reject();
         })
-        .on('end', () => {
-          console.log(`${habitablePlanets.length} habitable planets found!`);
+        .on('end', async () => {
+          const countPlanetsFound = (await getAllPlanets()).length;
+          console.log(`${countPlanetsFound} habitable planets found!`);
           resolve();
         });  
     });
 };
 
-function getAllPlanets() {
-  return habitablePlanets;
+async function getAllPlanets() {
+  return await planets.find({});
 };
+
+async function savePlanet(planet) {
+  try {
+    await planets.updateOne({
+      keplerName: planet.kepler_name,
+    }, {
+      keplerName: planet.kepler_name,
+    }, {
+      upsert: true,
+    });
+  } catch(err) {
+    console.error(`couldn't save planet ${err}`);
+  }
+}
 
 
 

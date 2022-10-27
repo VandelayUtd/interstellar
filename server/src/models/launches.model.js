35f59fdd531d485a05ac7555f7f6e05a3d1b48ae
VandelayUtd <drewhadley.guitar@gmail.com>
2022-10-27 16:48:10 -0400

    launches post route now successfully posts entered form data to the database

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,9 +1,10 @@
 const launchesDatabase = require('./launches.mongo');
 const planets = require('./planets.mongo');
 
+const DEFAULT_FLIGHT_NUMBER = 100;
+
 const launches = new Map();
 
-let latestFlightNumber = 100;
 
 const launch = { 
     flightNumber: 100,
@@ -22,6 +23,18 @@ saveLaunch(launch);
 function existsLaunchWithId(launchId) {
     return launches.has(launchId);
 }
+
+async function getLatestFlightNumber() {
+    const latestLaunch = await launchesDatabase
+        .findOne()
+        .sort('-flightNumber');
+
+    if(!latestLaunch) {
+        return DEFAULT_FLIGHT_NUMBER;
+    }
+
+    return latestLaunch.flightNumber;
+};
 
 async function getAllLaunches() {
     return await launchesDatabase
@@ -45,18 +58,19 @@ async function saveLaunch(launch) {
     });
 };
 
-function addNewLaunch(launch) {
-    latestFlightNumber ++;
-    launches.set(
-        latestFlightNumber, 
-        Object.assign(launch, {
-            success: true,
-            upcoming: true,
-            customer: ['Edward', 'NASA'],
-            flightNumber: latestFlightNumber,
-        })
-    );
-};
+async function scheduleNewLaunch(launch) {
+    const newFlightNumber = await getLatestFlightNumber() + 1;
+
+    const newLaunch = Object.assign(launch, {
+        success: true,
+        upcoming: true,
+        customer: ['Drew', 'NASA'],
+        flightNumber: newFlightNumber, 
+    });
+
+    await saveLaunch(newLaunch)
+}
+
 
 function abortLaunchById(launchId) {
     const aborted = launches.get(launchId);
@@ -68,6 +82,6 @@ function abortLaunchById(launchId) {
 module.exports = {
     existsLaunchWithId,
     getAllLaunches,
-    addNewLaunch,
+    scheduleNewLaunch,
     abortLaunchById,
 };

bbad583d9702d58e718fa5c2ff60829287357da4
VandelayUtd <drewhadley.guitar@gmail.com>
2022-11-14 15:06:08 -0500

    built seperate function to populate launch data when loadLaunches is called and added conditions to optimize efficiency

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -24,7 +24,7 @@ saveLaunch(launch);
 
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query'
 
-async function loadLaunchData() {
+async function populateLaunches() { 
     console.log('downloading launch data...');
     const response = await axios.post(SPACEX_API_URL, {
         query: {},
@@ -64,11 +64,33 @@ async function loadLaunchData() {
             customers,
         };
         console.log(`${launch.flightNumber} ${launch.mission}`)
+
+        //TODO: populate launches collection
     }
 }
 
+async function loadLaunchData() {
+    const firstLaunch = await findLaunch({
+        flightNumber: 1,
+        rocket: 'Falcon 1',
+        mission: 'FalconSat',
+    });
+
+    if (firstLaunch) {
+        console.log('Launch data already loaded');
+    } else {
+        await populateLaunches();
+    }
+
+   
+}
+
+async function findLaunch(filter) {
+    return await launchesDatabase.findOne(filter);
+}
+
 async function existsLaunchWithId(launchId) {
-    return await launchesDatabase.findOne({
+    return await findLaunch({
         flightNumber: launchId,
     });
 };

bf2a1b9b4f082c0edd7065b8d80f6488efd2e879
VandelayUtd <drewhadley.guitar@gmail.com>
2023-01-24 13:46:45 -0500

    adjusted launch test and added npm test back to yml

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../../app');
+const { loadPlanetsData } = require('../../models/planets.model');
 const { 
     mongoConnect, 
     mongoDisconnect, 
@@ -9,6 +10,7 @@ describe('Launches API', () => {
 
     beforeAll(async () => {
         await mongoConnect();
+        await loadPlanetsData();
     });
 
     afterAll(async () => {
